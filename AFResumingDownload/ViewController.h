14f133f195360086f461969b7019a2b2ec4675b8
reference <imti_bandianhong@126.com>
2012-09-21 15:54:26 +0800

    Initial Commit

---
+++ AFResumingDownload/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AFResumingDownload
+//
+//  Created by sban@netspectrum.com on 9/21/12.
+//  Copyright (c) 2012 Netspectrum Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AFResumingDownload/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  AFResumingDownload
+//
+//  Created by sban@netspectrum.com on 9/21/12.
+//  Copyright (c) 2012 Netspectrum Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
